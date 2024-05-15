067f8d4acb5c6530c2997abe00f284a825a02ee2
claireamira <clairenkatha360@gmail.com>
2024-05-15 12:02:28 +0300

    Tests

---
+++ test1.js
@@ -0,0 +1,26 @@
+let x = 35
+{
+    console.log(x)
+    x = 45
+    console.log(x)
+    let y = 600
+    console.log(y)
+}
+{
+    console.log(x)
+    const m = 3.111
+    console.log(m)
+}
+{
+    let l = 10
+    let w = 20
+    let area = l * w
+    console.log(area)
+}
+{
+    let d = 20
+    let r = d / 2 //Finding the radius
+    const pi = 3.14
+    let area1 = pi * (r**r)
+    console.log(area1)
+}
